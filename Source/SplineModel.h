03d5c845f9ff913b0e4968615555b9e989b6cea4
kyle-ilantzis <ilantzis.kyle@gmail.com>
2015-08-02 14:39:29 -0400

    PlayerModel checks the range of the spline when moving

--- Source/SplineModel.h
+++ Source/SplineModel.h
@@ -33,7 +33,7 @@ public:
 
 	Plane PlaneAt(float t);
 
-	int MaxTime() { return mControlPoints.size() - 4; }
+	float MaxTime() { return mControlPoints.size() - 4; }
 
 	bool HasControlPoints() { return !mControlPoints.empty(); }
 	std::vector<Vertex>& GetControlPoints() { return mControlPoints; }
