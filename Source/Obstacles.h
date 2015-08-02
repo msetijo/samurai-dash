33d290ef837efd085e2af81a009ef039e83244e6
Robert Wen <robertwen20@gmail.com>
2015-08-02 19:14:50 -0400

    Removed from the header

--- Source/Obstacles.h
+++ Source/Obstacles.h
@@ -14,9 +14,7 @@ class Obstacles
 public:
 	std::vector < Model* > listObstacles;
 	Obstacles();
-	void RemoveObstacles(int position);
 	glm::vec3 RandomizeTrack(float t);
-	void AddObstacles(Model* m);
 	void PopulateRandomSample();
 	void Draw();
 	Model* GetRandomModel();
